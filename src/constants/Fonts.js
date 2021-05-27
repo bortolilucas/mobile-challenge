231d6d9ef06b8dbbe1f6fdb1362fc694fd651545
lucas <lucasbortolidev@gmail.com>
2021-05-27 17:25:35 -0300

    chore: add fonts

---
+++ src/constants/Fonts.js
@@ -0,0 +1,8 @@
+export default {
+  BLACK: 'Montserrat-Black',
+  BOLD: 'Montserrat-Bold',
+  EXTRA_BOLD: 'Montserrat-ExtraBold',
+  MEDIUM: 'Montserrat-Medium',
+  REGULAR: 'Montserrat-Regular',
+  SEMI_BOLD: 'Montserrat-SemiBold',
+};
