8b032514bb52e5b44089afa16c31fa718607b29e
Lucas Bortoli <73895706+bortolilucas@users.noreply.github.com>
2021-06-23 11:21:52 -0300

    perf(utils): remove unnecessary condition

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -10,7 +10,6 @@ export const isEmail = value => {
 
 export function isEmpty(value) {
   if (!value) return true;
-  if (value === undefined) return true;
   if (Array.isArray(value) && value.length === 0) return true;
   if (typeof value === 'object' && Object.keys(value).length === 0) return true;
   return false;
